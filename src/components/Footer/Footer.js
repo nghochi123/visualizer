9d74b8c133e2b9675bb18b60eefd865bd364c07f
nghochi123 <nghochi123@gmail.com>
2021-04-10 20:28:28 +0800

    fixing some small stuff

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,9 @@
 import React, {useContext} from 'react';
 import {Container, Grid, Link} from '@material-ui/core';
 
-import {GlobalStateContext} from '../../context/GlobalContextProvider'; 
+import {GlobalStateContext} from '../../context/GlobalContextProvider';
+
+import * as styles from './Footer.module.css';
 
 const Footer = (props) => {
     const state = useContext(GlobalStateContext);
@@ -14,12 +16,12 @@ const Footer = (props) => {
                 justify="flex-start"
                 alignItems="center"
             >
-                <Grid item xs={8} spacing={3}>
-                    <p>© 2021 by Ho Chi</p>
+                <Grid item xs={6} spacing={3}>
+                    <p className={styles.smallerscreen}>© 2021 by Ho Chi</p>
                 </Grid>
-                <Grid item xs={4} spacing={3} style={{textAlign: 'right'}}>
-                    <Link color={theme} href="https://github.com/nghochi123/visualizer">Source Code</Link>
-                    <Link color={theme} href="https://visualgo.net/en" style={{marginLeft: '10px'}}>Inspiration</Link>
+                <Grid item xs={6} spacing={3} style={{textAlign: 'right'}}>
+                    <Link className={styles.smallerscreen} color={theme} href="https://github.com/nghochi123/visualizer">Source Code</Link>
+                    <Link className={styles.smallerscreen} color={theme} href="https://visualgo.net/en" style={{marginLeft: '10px'}}>Inspiration</Link>
                 </Grid>
             </Grid>
         </Container>
