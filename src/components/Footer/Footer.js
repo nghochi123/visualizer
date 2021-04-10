76d956dcbc7878e86137bb51b8b19922fcd7b24d
nghochi123 <nghochi123@gmail.com>
2021-04-10 11:59:24 +0800

    Added favicon and title, as well as edited footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import {Container, Grid} from '@material-ui/core';
+import React, {useContext} from 'react';
+import {Container, Grid, Link} from '@material-ui/core';
+
+import {GlobalStateContext} from '../../context/GlobalContextProvider'; 
 
 const Footer = (props) => {
+    const state = useContext(GlobalStateContext);
+    const theme = state.darkMode ? 'secondary' : 'primary';
     return (
         <Container>
             <Grid
                 container
                 direction="row"
-                justify="left"
+                justify="flex-start"
                 alignItems="center"
             >
                 <Grid item xs={8} spacing={3}>
                     <p>© 2021 by Ho Chi</p>
+                </Grid>
+                <Grid item xs={4} spacing={3} style={{textAlign: 'right'}}>
+                    <Link color={theme} href="https://github.com/nghochi123/visualizer">Source Code</Link>
+                    <Link color={theme} href="https://visualgo.net/en" style={{marginLeft: '10px'}}>Inspiration</Link>
                 </Grid>
             </Grid>
         </Container>
